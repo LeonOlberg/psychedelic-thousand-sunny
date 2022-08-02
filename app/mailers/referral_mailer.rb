3893d52e88765c7f5e3f78e3967645038bef1ac7
Leonardo Olberg <leo.olberg@gmail.com>
2022-08-02 15:41:11 -0300

    Fix rubocop offenses

--- app/mailers/referral_mailer.rb
+++ app/mailers/referral_mailer.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class ReferralMailer < ApplicationMailer
 
   def welcome(referral_id:, referral_email:)
     mail(to: referral_email, subject: 'You have been referred in our platform')
 
-    Event.create(kind: :referral_email_sent, reference: { 'referral_id' => referral_id } )
+    Event.create(kind: :referral_email_sent, reference: { 'referral_id' => referral_id })
   end
 end
--- app/models/referral.rb
+++ app/models/referral.rb
@@ -20,10 +20,10 @@ class Referral < ApplicationRecord
 
   def wellcome_mail_event_waiting_to_be_sent
     ReferralMailer.welcome(referral_id: id, referral_email: email).deliver_later
-    Event.create(kind: :referral_email_waiting_to_be_sent, reference: { 'referral_id' => id } )
+    Event.create(kind: :referral_email_waiting_to_be_sent, reference: { 'referral_id' => id })
   end
 
   def referral_deleted_event
-    Event.create(kind: :referral_deleted, reference: { 'referral_id' => id } )
+    Event.create(kind: :referral_deleted, reference: { 'referral_id' => id })
   end
 end
