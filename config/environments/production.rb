cb4689415b0a14ab0ff11ddac2110f9f23b3eab7
Leonardo Olberg <leo.olberg@gmail.com>
2022-08-02 21:42:02 -0300

    Rename project

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "psychedelic_thousand_sunny_production"
+  # config.active_job.queue_name_prefix = "referable_rails_production"
 
   config.action_mailer.perform_caching = false
 
